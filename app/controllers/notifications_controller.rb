guzara/fizzy

commit d3c5c86b217009e31d33cf531445fb4c4343cab8

Style

app/controllers/notifications_controller.rb
@@ -1,10 +1,10 @@
 class NotificationsController < ApplicationController
   def index
-    set_page_and_extract_portion_from Current.user.notifications.read.ordered
-
     if @page.first?
       @unread = Current.user.notifications.unread.ordered
     end
+
+    set_page_and_extract_portion_from Current.user.notifications.read.ordered
   end
 
   def mark_read

commit 289487a38362aa9a905729c8cb9ad0cbe7a1669c

Source order seems to be significant
Regressed in d3c5c86b217009e31d33cf531445fb4c4343cab8

app/controllers/notifications_controller.rb
@@ -1,10 +1,10 @@
 class NotificationsController < ApplicationController
   def index
+    set_page_and_extract_portion_from Current.user.notifications.read.ordered
+
     if @page.first?
       @unread = Current.user.notifications.unread.ordered
     end
-
-    set_page_and_extract_portion_from Current.user.notifications.read.ordered
   end
 
   def mark_read